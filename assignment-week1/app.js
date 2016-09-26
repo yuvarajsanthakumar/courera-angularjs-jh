8ca8d43841b1b355f3cbc07402df6b5246fa4757
Yuvaraj Santhakumar <yuvaraj89@gmail.com>
2016-09-26 21:23:40 +0530

    first assignment completed

---
+++ assignment-week1/app.js
@@ -0,0 +1,34 @@
+(function () {
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.inputText ;
+  $scope.outputText;
+
+  $scope.checkLunch = function(){
+    if ($scope.inputText === undefined || $scope.inputText==="") {
+      $scope.outputText = "Please enter data first";
+    } else {
+        var arrayOfFood = $scope.inputText.split(',');
+        var items = arrayOfFood.filter(ValidItem)
+        if (items.length <= 3) {
+          $scope.outputText = "Enjoy!";
+        } else {
+            $scope.outputText = "Too much!";
+        }
+    }
+  }
+
+  function ValidItem(item) {
+    if(item===undefined || item.trim()===""){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  }
+}());
