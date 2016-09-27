08e54bdd6c88d2098ef71bbddd200bad1a3e9470
yuvarajsanthakumar <yuvaraj89@gmail.com>
2016-09-27 22:58:35 +0530

    Update app.js

--- assignment-week1/app.js
+++ assignment-week1/app.js
@@ -5,7 +5,7 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope) {
-  $scope.inputText="";
+  $scope.inputText="1";
   $scope.outputText;
 
   $scope.checkLunch = function(){
